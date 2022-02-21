e15764d91b2d2750ea7ee4ccc2200423e2ed895f
obsesiva-syntaxis <obsesivasyntaxis@gmail.com>
2022-02-21 19:54:14 -0300

    some changes to landing

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,55 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './Landing.scss';
 
 export default function Landing() {
+    const navigate = useNavigate();
 
-
-    const handleLogin = () => {
+    const clickBtn = () => {
+        navigate('/login');
     }
 
     return (
-        <div class="landing">
-            <div class="landing__banner1"></div>
-            <div class="landing__banner2"></div>
-            <div class="landing__banner3"></div>
-            <div class="landing__banner4"></div>
-            <div class="landing__banner5"></div>
-            <div class="landing__banner6"></div>
-            <div class="landing__banner7"></div>
-            <div class="landing__banner8"></div>
-            <div class="landing__banner9">
-                <h1 class="title">K</h1>
+        <div className="landing">
+            <div className="landing__banner1"></div>
+            <div className="landing__banner2"></div>
+            <div className="landing__banner3"></div>
+            <div className="landing__banner4"></div>
+            <div className="landing__banner5"></div>
+            <div className="landing__banner6"></div>
+            <div className="landing__banner7"></div>
+            <div className="landing__banner8"></div>
+            <div className="landing__banner9">
+                <h1 className="title" onClick={ clickBtn }>K</h1>
             </div>
-            <div class="landing__banner10">
-                <h1 class="title">U</h1>
+            <div className="landing__banner10">
+                <h1 className="title" onClick={ clickBtn }>U</h1>
             </div>
-            <div class="landing__banner11">
-                <h1 class="title">R</h1>
+            <div className="landing__banner11">
+                <h1 className="title" onClick={ clickBtn }>R</h1>
             </div>
-            <div class="landing__banner12">
-                <h1 class="title">E</h1>
+            <div className="landing__banner12">
+                <h1 className="title" onClick={ clickBtn }>E</h1>
             </div>
-            <div class="landing__banner13">
-                <h1 class="title">N</h1>
+            <div className="landing__banner13">
+                <h1 className="title" onClick={ clickBtn }>N</h1>
             </div>
-            <div class="landing__banner14">
-                <h1 class="title">A</h1>
+            <div className="landing__banner14">
+                <h1 className="title" onClick={ clickBtn }>A</h1>
             </div>
-            <div class="landing__banner15">
-                <h1 class="title">I</h1>
+            <div className="landing__banner15">
+                <h1 className="title" onClick={ clickBtn }>I</h1>
             </div>
-            <div class="landing__banner16">
+            <div className="landing__banner16">
             </div>
-            <div class="landing__banner17"></div>
-            <div class="landing__banner18"></div>
-            <div class="landing__banner19"></div>
-            <div class="landing__banner20"></div>
-            <div class="landing__banner21"></div>
-            <div class="landing__banner22"></div>
-            <div class="landing__banner23"></div>
-            <div class="landing__banner24"></div>
-            <div class="landing__banner26" onClick={ handleLogin }></div>
+            <div className="landing__banner17"></div>
+            <div className="landing__banner18"></div>
+            <div className="landing__banner19"></div>
+            <div className="landing__banner20"></div>
+            <div className="landing__banner21"></div>
+            <div className="landing__banner22"></div>
+            <div className="landing__banner23"></div>
+            <div className="landing__banner24"></div>
+            <div className="landing__banner26"/>
+            {/* <button className="landing__banner26" onClick={ clickBtn }></button> */}
         </div>
     )
 }
